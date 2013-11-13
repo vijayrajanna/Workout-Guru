a43458b44c5ff69eee204bbf92304b5e102e9acf
Lalita <lalita.sattavat@gmail.com>
2013-11-12 20:53:03 -0600

    Login + Register Screen changes

--- gen/com/example/workoutguru/R.java
+++ gen/com/example/workoutguru/R.java
@@ -8,6 +8,13 @@
 package com.example.workoutguru;
 
 public final class R {
+    public static final class array {
+        public static final int genderList=0x7f060000;
+        public static final int pref_example_list_titles=0x7f060001;
+        public static final int pref_example_list_values=0x7f060002;
+        public static final int pref_sync_frequency_titles=0x7f060003;
+        public static final int pref_sync_frequency_values=0x7f060004;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,26 +31,105 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f080008;
-        public static final int editTextX=0x7f080005;
-        public static final int editTextY=0x7f080003;
-        public static final int editTextZ=0x7f080007;
-        public static final int labelAccData=0x7f080001;
-        public static final int labelXaxis=0x7f080004;
-        public static final int labelYaxis=0x7f080002;
-        public static final int labelZaxis=0x7f080006;
+        public static final int LinearLayout1=0x7f090009;
+        public static final int action_forgot_password=0x7f09001f;
+        public static final int action_settings=0x7f090020;
+        public static final int checkBox1=0x7f090006;
+        public static final int editTextX=0x7f09000e;
+        public static final int editTextY=0x7f09000c;
+        public static final int editTextZ=0x7f090010;
+        public static final int email=0x7f090003;
+        public static final int labelAccData=0x7f09000a;
+        public static final int labelActivity=0x7f090011;
+        public static final int labelXaxis=0x7f09000d;
+        public static final int labelYaxis=0x7f09000b;
+        public static final int labelZaxis=0x7f09000f;
+        public static final int login=0x7f090005;
+        public static final int login_form=0x7f090002;
+        public static final int login_status=0x7f090000;
+        public static final int login_status_message=0x7f090001;
+        public static final int password=0x7f090004;
+        public static final int register=0x7f09001e;
+        public static final int register_button=0x7f090008;
+        public static final int sign_in_button=0x7f090007;
+        public static final int spinner1=0x7f090019;
+        public static final int textView1=0x7f090016;
+        public static final int textView2=0x7f090015;
+        public static final int textView3=0x7f090018;
+        public static final int textView4=0x7f090017;
+        public static final int textView5=0x7f09001a;
+        public static final int textView6=0x7f09001c;
+        public static final int txtAge=0x7f090014;
+        public static final int txtHeight=0x7f09001d;
+        public static final int txtPassword=0x7f090013;
+        public static final int txtUsername=0x7f090012;
+        public static final int txtWeight=0x7f09001b;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_register_user=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int login=0x7f080000;
+        public static final int main=0x7f080001;
+        public static final int register_user=0x7f080002;
     }
     public static final class string {
+        public static final int accelerometerData=0x7f050003;
+        public static final int action_forgot_password=0x7f05000c;
+        public static final int action_register=0x7f05000a;
         public static final int action_settings=0x7f050001;
+        public static final int action_sign_in_short=0x7f05000b;
+        public static final int age=0x7f050016;
         public static final int app_name=0x7f050000;
+        public static final int cancel=0x7f05001b;
+        public static final int error_field_required=0x7f050011;
+        public static final int error_incorrect_password=0x7f050010;
+        public static final int error_invalid_email=0x7f05000e;
+        public static final int error_invalid_password=0x7f05000f;
+        public static final int gender=0x7f050017;
+        public static final int height=0x7f050019;
         public static final int hello_world=0x7f050002;
+        public static final int login_progress_signing_in=0x7f05000d;
+        public static final int pref_default_display_name=0x7f050021;
+        public static final int pref_description_social_recommendations=0x7f05001f;
+        /**  Example settings for Data & Sync 
+         */
+        public static final int pref_header_data_sync=0x7f050023;
+        /**  Strings related to Settings 
+ Example General settings 
+         */
+        public static final int pref_header_general=0x7f05001d;
+        /**  Example settings for Notifications 
+         */
+        public static final int pref_header_notifications=0x7f050026;
+        public static final int pref_ringtone_silent=0x7f050029;
+        public static final int pref_title_add_friends_to_messages=0x7f050022;
+        public static final int pref_title_display_name=0x7f050020;
+        public static final int pref_title_new_message_notifications=0x7f050027;
+        public static final int pref_title_ringtone=0x7f050028;
+        public static final int pref_title_social_recommendations=0x7f05001e;
+        public static final int pref_title_sync_frequency=0x7f050024;
+        public static final int pref_title_system_sync_settings=0x7f050025;
+        public static final int pref_title_vibrate=0x7f05002a;
+        public static final int prompt_email=0x7f050008;
+        public static final int prompt_password=0x7f050009;
+        public static final int rememberMe=0x7f050012;
+        public static final int save=0x7f05001a;
+        /**  Strings related to Register User 
+         */
+        public static final int signup=0x7f050014;
+        /**  Strings related to login 
+         */
+        public static final int title_activity_login=0x7f050007;
+        public static final int title_activity_register_user=0x7f050013;
+        public static final int title_activity_settings=0x7f05001c;
+        public static final int username=0x7f050015;
+        public static final int weight=0x7f050018;
+        public static final int xAxis=0x7f050004;
+        public static final int yAxis=0x7f050006;
+        public static final int zAxis=0x7f050005;
     }
     public static final class style {
         /** 
@@ -56,10 +142,11 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int LoginFormContainer=0x7f070002;
     }
 }
